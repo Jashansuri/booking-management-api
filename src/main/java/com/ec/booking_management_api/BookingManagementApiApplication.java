32ceb8efec581d515bc67098f4bd35a909f25244
Jashan Suri <jashansuri@gmail.com>
2026-01-31 11:25:21 +1000

    Initial commit

---
+++ src/main/java/com/ec/booking_management_api/BookingManagementApiApplication.java
@@ -0,0 +1,13 @@
+package com.ec.booking_management_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookingManagementApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookingManagementApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ec/booking_management_api/BookingManagementApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ec.booking_management_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookingManagementApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
